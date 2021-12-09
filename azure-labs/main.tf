097fa7623e38c89301a3f09aa227de2981e5da75
Matthew Tan <matthew.tan@ca.pwc.com>
2021-12-09 14:16:24 -0500

    matt test

--- azure-labs/main.tf
+++ azure-labs/main.tf
@@ -8,14 +8,14 @@ data "azurerm_management_group" "cloud_eng_labs" {
     name = var.azure_root_management_group
 }
 
-# resource "azurerm_subscription" "team" {
-#     for_each          = var.all_users
-#     subscription_name = each.key
-#     billing_scope_id  = data.azurerm_billing_mca_account_scope.main.id
-# }
+resource "azurerm_subscription" "team" {
+     for_each          = var.all_users
+     subscription_name = each.key
+     billing_scope_id  = data.azurerm_billing_mca_account_scope.main.id
+ }
 
-# resource "azurerm_management_group_subscription_association" "team" {
-#     for_each            = var.all_users
-#     management_group_id = data.azurerm_management_group.cloud_eng_labs.id
-#     subscription_id     = azurerm_subscription.team[each.key].id
-# }
+resource "azurerm_management_group_subscription_association" "team" {
+     for_each            = var.all_users
+     management_group_id = data.azurerm_management_group.cloud_eng_labs.id
+     subscription_id     = azurerm_subscription.team[each.key].id
+ }
